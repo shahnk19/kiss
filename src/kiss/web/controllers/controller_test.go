27062896a6ed7126a66e581e6b3e923b298bfa1f
cention-shah <shah.khan@cention.se>
2017-09-22 19:49:54 +0800

    Various test the 'Encode' function with a mock db

--- src/kiss/web/controllers/controller_test.go
+++ src/kiss/web/controllers/controller_test.go
@@ -2,13 +2,17 @@ package controllers
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"kiss/web/models"
 	"net/http"
 	"net/http/httptest"
-	"strconv"
-	"strings"
 	"testing"
+)
+
+const (
+	apiPrefix = "/api/encode?url=https://www.test.com/"
 )
 
 //test any url validation that is not covered by golang lib url.ParseRequestURI
@@ -28,57 +32,78 @@ func TestIsValidUrl(t *testing.T) {
 	}
 }
 
-//test the endponts
-type mockModel struct{}
-
-func (m *mockModel) NewEntry(surl string) (int, error) {
-	sa := strings.Split(surl, "https://www.test.com/")
-	salen := len(sa)
-	if salen > 0 {
-		i, _ := strconv.Atoi(sa[salen-1])
-		return i, nil
-	}
-	return 0, nil
+//dummy models
+type mockModel struct {
+	lastId    int
+	currentId int
 }
 
-func TestEndpointsEncode(t *testing.T) {
-	apiPrefix := "/api/encode?url=https://www.test.com/"
-	testCases := []struct {
-		what string
-		want VM
-	}{
-		{"1", VM{Value: "1", Status: true, Error: ""}},
-		{"1001", VM{Value: "3e9", Status: true, Error: ""}},
+func (m *mockModel) SaveTiny(surl, id string) (string, error) {
+	if m.currentId > 0 && m.currentId > m.lastId {
+		m.lastId++
+		return "", errors.New("Id collision mock")
 	}
-	var got VM
-	for _, ttc := range testCases {
-		rec := runHttpTestRequests(apiPrefix + ttc.what)
-		err := json.Unmarshal(rec.Body.Bytes(), &got)
-		if err != nil {
-			t.Error(fmt.Sprintf("(!!!)Fail (what:%v,want:%v,got:%v)", ttc.what, ttc.want, got))
-		}
-		if ttc.want != got {
-			t.Error(fmt.Sprintf("(!!!)Fail (what:%v,want:%v,got:%v)", ttc.what, ttc.want, got))
-		}
+	return id, nil
+}
+
+func (m *mockModel) GetLastId() int {
+	m.lastId++
+	return m.lastId
+}
+
+func TestEndpointsEncode_Basic(t *testing.T) {
+	want := VM{Value: "1", Status: true, Error: ""}
+	got := runHttpTestRequests(t, apiPrefix, &mockModel{lastId: 0})
+	if want != got {
+		t.Error(fmt.Sprintf("(!!!)Fail (want:%v,got:%v)", want, got))
 	}
 }
 
-func runHttpTestRequests(endpoint string) *httptest.ResponseRecorder {
-	ctrl := getTestController()
+func TestEndpointsEncode_LastIdAlreadyExists(t *testing.T) { //will force retry 1 times
+	want := VM{Value: "2", Status: true, Error: ""}
+	got := runHttpTestRequests(t, apiPrefix, &mockModel{lastId: 1, currentId: 2})
+	if want != got {
+		t.Error(fmt.Sprintf("(!!!)Fail (want:%v,got:%v)", want, got))
+	}
+}
+
+func TestEndpointsEncode_LastIdAlreadyExistsLastRetry(t *testing.T) { //force to go to the last retry
+	want := VM{Value: "2", Status: true, Error: ""}
+	got := runHttpTestRequests(t, apiPrefix, &mockModel{lastId: 1, currentId: 6})
+	if want != got {
+		t.Error(fmt.Sprintf("(!!!)Fail (want:%v,got:%v)", want, got))
+	}
+}
+
+func TestEndpointsEncode_LastIdAlreadyExistsExhaustRetry(t *testing.T) { //exhaust all retry
+	want := VM{Value: "", Status: false, Error: ERR_NEW_ENTRY_FAIL}
+	got := runHttpTestRequests(t, apiPrefix, &mockModel{lastId: 1, currentId: 7})
+	if want != got {
+		t.Error(fmt.Sprintf("(!!!)Fail (want:%v,got:%v)", want, got))
+	}
+}
+
+func runHttpTestRequests(t *testing.T, endpoint string, model models.IModel) VM {
+	ctrl := getTestController(model)
 	router := getTestRouter(ctrl)
 	rec := httptest.NewRecorder()
 	req, _ := http.NewRequest("GET", endpoint, nil)
 	router.ServeHTTP(rec, req)
-	return rec
+	var got VM
+	err := json.Unmarshal(rec.Body.Bytes(), &got)
+	if err != nil {
+		t.Error(fmt.Sprintf("(!!!)Fail unmarshall response :%v", err))
+	}
+	return got
 }
 
-func getTestController() *Ctrl {
+func getTestController(model models.IModel) *Ctrl {
 	enc := getBaseEncoder()
 
 	return &Ctrl{
 		Name:    "mock",
 		encoder: enc,
-		model:   &mockModel{},
+		model:   model, //&mockModel{lastId: 0},
 	}
 }
 
